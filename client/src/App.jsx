f47bffc037236d5f57e478d8ca5486efd498ee6b
Zach <zachh85@gmail.com>
2022-12-09 16:51:06 -0500

    commit main files

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import '../public/styles.scss';
+
+function App() {
+
+  return (
+    <div>
+
+// route and switches go here
+
+    </div>
+    
+  );
+}
+
+export default App;
